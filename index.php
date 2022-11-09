c55717f9cfa5fad8e81fadf299d52ff1eb67d06b
GrapeApple <budouapplenoki@gmail.com>
2022-11-09 17:24:52 +0900

    init

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+	<link rel="stylesheet" href="style.css">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+	<title>micore</title>
+</head>
+
+<body>
+	<header>
+		<nav style="position: fixed; ">
+			<div class="nav-wrapper">
+				<a href="#!" class="brand-logo">micore</a>
+			</div>
+		</nav>
+	</header>
+	<div class="container">
+		<div class="section">
+			<div class="row" style="margin-top: 10%;">
+				<div class="col s12 card-parent">
+					<div class="card red lighten-5">
+						<div class="card-content">
+							<span class="card-title">テスト</span>
+							<p>なんかとっても便利で役に立つかもしれないもの</p>
+						</div>
+						<div class="card-action">
+							<a style="color: #f48fb1" href="#">使ってみる</a>
+						</div>
+					</div>
+				</div>
+				<div class="col s12 card-parent">
+					<div class="card red lighten-5">
+						<div class="card-content">
+							<span class="card-title">テスト</span>
+							<p>なんかとっても便利で役に立つかもしれないもの</p>
+						</div>
+						<div class="card-action">
+							<a style="color: #f48fb1" href="#">使ってみる</a>
+						</div>
+					</div>
+				</div>
+				<div class="col s12 card-parent">
+					<div class="card red lighten-5">
+						<div class="card-content">
+							<span class="card-title">テスト</span>
+							<p>なんかとっても便利で役に立つかもしれないもの</p>
+						</div>
+						<div class="card-action">
+							<a style="color: #f48fb1" href="#">使ってみる</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+
+</html>
